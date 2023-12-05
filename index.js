9d8017ad0326c6ccbe6428732e0059c9b1cf1723
Eric198809 <150351199+Eric198809@users.noreply.github.com>
2023-12-05 12:14:53 +0100

    Ok index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cowsay = require("cowsay");
 
 console.log(
   cowsay.say({
-    text: "je m appelle Eric et je viens du campus de Lyon",
+    text: "je m appelle ${person.name} et je viens du campus de ${person.campus}",
     e: "oO",
     T: "U ",
   })
